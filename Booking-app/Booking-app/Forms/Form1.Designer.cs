2515711145014cde82d36ac665481268c326a763
Kevin David <kevindavidvenyk@gmail.com>
2025-12-02 00:45:14 +0200

    init

---
+++ Booking-app/Booking-app/Forms/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Booking_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainFlow = new System.Windows.Forms.FlowLayoutPanel();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.profileButton = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainFlow
+            // 
+            this.mainFlow.AutoScroll = true;
+            this.mainFlow.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.mainFlow.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.mainFlow.Location = new System.Drawing.Point(0, 70);
+            this.mainFlow.Margin = new System.Windows.Forms.Padding(0);
+            this.mainFlow.Name = "mainFlow";
+            this.mainFlow.Padding = new System.Windows.Forms.Padding(20);
+            this.mainFlow.Size = new System.Drawing.Size(1268, 612);
+            this.mainFlow.TabIndex = 0;
+            this.mainFlow.WrapContents = false;
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(1145, 12);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(107, 34);
+            this.logoutButton.TabIndex = 1;
+            this.logoutButton.Text = "Log Out";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            // 
+            // profileButton
+            // 
+            this.profileButton.Location = new System.Drawing.Point(1032, 13);
+            this.profileButton.Name = "profileButton";
+            this.profileButton.Size = new System.Drawing.Size(107, 34);
+            this.profileButton.TabIndex = 2;
+            this.profileButton.Text = "Profile";
+            this.profileButton.UseVisualStyleBackColor = true;
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(479, 8);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(258, 38);
+            this.name.TabIndex = 3;
+            this.name.Text = "Ventura Club";
+            this.name.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.profileButton);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.mainFlow);
+            this.Name = "Form1";
+            this.Text = "Home Page";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel mainFlow;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Button profileButton;
+        private System.Windows.Forms.Label name;
+    }
+}
+
---
+++ Booking-app/Booking-app/Forms/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Booking_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Panel CreateCard(string name, Image img = null)
+        {
+            Panel card = new Panel
+            {
+                Width = 180,
+                Height = 150,
+                BackColor = Color.White,
+                BorderStyle = BorderStyle.FixedSingle,
+                Margin = new Padding(10),
+                Cursor = Cursors.Hand
+            };
+
+            PictureBox pb = new PictureBox
+            {
+                Dock = DockStyle.Top,
+                Height = 100,
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Image = img
+            };
+
+            Label label = new Label
+            {
+                Text = name,
+                Dock = DockStyle.Bottom,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Height = 30
+            };
+
+            card.Controls.Add(label);
+            card.Controls.Add(pb);
+
+            card.Click += (s, e) => OnCardClick(name);
+            pb.Click += (s, e) => OnCardClick(name);
+            label.Click += (s, e) => OnCardClick(name);
+
+            card.MouseEnter += (s, e) => card.BackColor = Color.FromKnownColor(KnownColor.ControlDark);
+            pb.MouseEnter += (s, e) => card.BackColor = Color.FromKnownColor(KnownColor.ControlDark);
+            label.MouseEnter += (s, e) => card.BackColor = Color.FromKnownColor(KnownColor.ControlDark);
+            card.MouseLeave += (s, e) => card.BackColor = Color.White;
+            pb.MouseLeave += (s, e) => card.BackColor = Color.White;
+            label.MouseLeave += (s, e) => card.BackColor = Color.White;
+
+            return card;
+        }
+
+        private void OnCardClick(string name)
+        {
+            MessageBox.Show($"You clicked on {name}");
+        }
+
+        private void addFacilitySection(string title, List<string> items)
+        {
+            Label label = new Label
+            {
+                Text = title,
+                Font = new Font("Segoe UI", 16, FontStyle.Bold),
+                AutoSize = false,
+                Height = 40,
+                Dock = DockStyle.Top,
+                Padding = new Padding(10, 5, 0, 5),
+                Margin = new Padding(0, 0, 0, 10)
+            };
+
+            FlowLayoutPanel flowLayoutPanel = new FlowLayoutPanel
+            {
+                FlowDirection = FlowDirection.LeftToRight,
+                WrapContents = true,
+                AutoSize = true,
+                Margin = new Padding(0, 0, 0, 30),
+                Padding = new Padding(10)
+            };
+
+            foreach (var item in items)
+            {
+                flowLayoutPanel.Controls.Add(CreateCard(item));
+            }
+            mainFlow.Controls.Add(label);
+            mainFlow.Controls.Add(flowLayoutPanel);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            addFacilitySection("Football Courts:", new List<string>
+                {
+                    "Court 1", "Court 2", "Court 3","Court 4", "Court 5", "Court 6", "Court 7"
+                });
+
+            addFacilitySection("Tennis Courts:", new List<string>
+                {
+                    "Court A", "Court B"
+                });
+
+            addFacilitySection("Saunas:", new List<string>
+                {
+                    "Sauna A", "Sauna B", "Sauna C"
+                });
+
+            addFacilitySection("Squash Courts:", new List<string>
+                {
+                    "Court A", "Court B", "Court C", "Court D"
+                });
+        }
+    }
+}
