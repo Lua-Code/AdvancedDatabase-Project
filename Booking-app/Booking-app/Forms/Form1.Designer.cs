7a5a72c219267286af2d02a840aa8980f338d209
Kevin David <kevindavidvenyk@gmail.com>
2025-12-02 13:30:11 +0200

    done

--- Booking-app/Booking-app/Forms/Form1.Designer.cs
+++ Booking-app/Booking-app/Forms/Form1.Designer.cs
@@ -32,18 +32,20 @@
             this.logoutButton = new System.Windows.Forms.Button();
             this.profileButton = new System.Windows.Forms.Button();
             this.name = new System.Windows.Forms.Label();
+            this.myBookingsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainFlow
             // 
             this.mainFlow.AutoScroll = true;
+            this.mainFlow.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.mainFlow.BackColor = System.Drawing.SystemColors.ControlLight;
             this.mainFlow.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.mainFlow.Location = new System.Drawing.Point(0, 70);
             this.mainFlow.Margin = new System.Windows.Forms.Padding(0);
             this.mainFlow.Name = "mainFlow";
             this.mainFlow.Padding = new System.Windows.Forms.Padding(20);
-            this.mainFlow.Size = new System.Drawing.Size(1268, 612);
+            this.mainFlow.Size = new System.Drawing.Size(1265, 612);
             this.mainFlow.TabIndex = 0;
             this.mainFlow.WrapContents = false;
             // 
@@ -68,18 +70,28 @@
             // name
             // 
             this.name.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.name.Location = new System.Drawing.Point(479, 8);
+            this.name.Location = new System.Drawing.Point(12, 8);
             this.name.Name = "name";
             this.name.Size = new System.Drawing.Size(258, 38);
             this.name.TabIndex = 3;
             this.name.Text = "Ventura Club";
-            this.name.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.name.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // myBookingsButton
+            // 
+            this.myBookingsButton.Location = new System.Drawing.Point(251, 13);
+            this.myBookingsButton.Name = "myBookingsButton";
+            this.myBookingsButton.Size = new System.Drawing.Size(107, 34);
+            this.myBookingsButton.TabIndex = 4;
+            this.myBookingsButton.Text = "My Bookings";
+            this.myBookingsButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.myBookingsButton);
             this.Controls.Add(this.name);
             this.Controls.Add(this.profileButton);
             this.Controls.Add(this.logoutButton);
@@ -98,6 +110,7 @@
         private System.Windows.Forms.Button logoutButton;
         private System.Windows.Forms.Button profileButton;
         private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Button myBookingsButton;
     }
 }
 
